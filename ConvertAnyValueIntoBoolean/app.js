0be8e1b50deedfda6fb431147fd05194429cf154
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-25 02:13:25 +0600

    Convert Into Bool

---
+++ ConvertAnyValueIntoBoolean/app.js
@@ -0,0 +1,9 @@
+
+
+
+console.log("azizar") // Azizar as a String 
+console.log(!'azizar') // Converted it into Boolean But it will be antonim of boolean which is false because it is not empty string.
+console.log(!!'azizar') //It's converted into boolean which is true because it is not empty string.
+
+//like
+console.log(!!50) // true Type of Boolean
